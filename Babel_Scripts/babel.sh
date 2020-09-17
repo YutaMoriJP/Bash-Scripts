0cb439ade3f005c8cecbca4e734d28b5bc7cc0e8
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2020-09-17 09:57:31 -0700

    added further details to the babel bash script

--- Babel_Scripts/babel.sh
+++ Babel_Scripts/babel.sh
@@ -6,8 +6,14 @@ echo 'Hi' $name
 #first we download package.json to set up the npm
 npm init
 
-#download node packages
+#download babel node packages:
 npm install babel-cli -D #downloads node pakcage with babel command lines
 npm install babel-preset-env -D #downloads node package that maps es6+ to es5
 
+#creates .babelrc file and adds an object to that file
 echo '{"presets": ["env"] }' >> .babelrc
+
+#after that, edit the package.json file, and the script (a key value pair) to the "scripts" object of package.json
+#"build": "babel src -d lib"
+
+#After adding {"build": "babel src -d lib"}, run the command npm run build
